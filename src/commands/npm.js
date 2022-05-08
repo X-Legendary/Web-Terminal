0a55d8c1775635e5a73b8f277874fb886046a9a1
X-Legendary <81035170+X-Legendary@users.noreply.github.com>
2022-05-08 18:39:04 +0200

    Reworked NPM Command

--- src/commands/npm.js
+++ src/commands/npm.js
@@ -1,11 +1,19 @@
 const npm = {
-    description: "Searches up the given package on npm",
-    usage: "npm <query>",
-    run: (commander, args) => {
+    description: "Get information on a npm package",
+    usage: "npm <package>",
+    run: async (commander, args) => {
         if(!Array.isArray(args)) return commander.updateLines("")
-        const query = args.join(" ")
-        commander.updateLines(`Searching ${query} on npm...`)
-        setTimeout(window.open(`https://npmjs.com/package/${encodeURIComponent(query)}`, "_self"), 250)
+        const packageName = args[0]
+        commander.updateLines(`Searching ${packageName} in npm's registry...`)
+        //setTimeout(window.open(`https://npmjs.com/package/${encodeURIComponent(query)}`, "_self"), 250)
+        const data = await fetch(`https://registry.npmjs.org/${encodeURIComponent(packageName)}`).then(r => r.json())
+        if(data.error) return commander.updateLines(`There was an error while trying to search npm's registry.\nError: ${data.error}`)
+
+        commander.updateLines(`${data.name} (v${data["dist-tags"].latest})`)
+        commander.updateLines(data.description)
+        commander.updateLines("") // empty line
+        commander.updateLines(<p><a href={data.homepage} target="_blank">Homepage</a>  •  <a href={data.bugs || "https://npmjs.com/package/" + data.name} target="_blank">Bugs</a> •  License: {data.license}</p>)
+        console.log(data)
     }
 }
 
